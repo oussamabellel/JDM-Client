32275a47c2f4f7416b0ac897844b74fcadd92cdd
Bellel Oussama <oussama.bellel@etu.umontpellier.fr>
2020-01-13 16:03:57 +0100

    historique

--- src/app/Components/result/result.component.ts
+++ src/app/Components/result/result.component.ts
@@ -19,6 +19,7 @@ export class ResultComponent implements OnInit {
   order_entrantes: string = 'poids';
   reverse: boolean = true;
   reverse_entrantes: boolean = true;
+  history: Array<string> = [];
 
 
   @Input() message: string;
@@ -41,20 +42,19 @@ export class ResultComponent implements OnInit {
   sendMessage(mot: string) {
     this.messageEvent.emit(mot)
   }
+
   ngOnInit() {
 
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['message'] !== undefined)
-      this.message = changes['message'].currentValue;
+  Search(message, relation) {
 
-    if (changes['relation'] !== undefined)
-      this.relation = changes['relation'].currentValue;
+    this.history.unshift(this.message);
+    localStorage.setItem('history', JSON.stringify(this.history));
 
     this.ngxLoader.startLoader('loader-01');
 
-    this.apiWord.getMot(this.message, this.relation).subscribe((res: Mot) => {
+    this.apiWord.getMot(message, relation).subscribe((res: Mot) => {
 
       this.res = res;
 
@@ -76,11 +76,19 @@ export class ResultComponent implements OnInit {
         }
         this.SortEmpty = Object.keys(this.res.mapSortantes);
       }
-
       this.ngxLoader.stopLoader('loader-01');
-
     })
+  }
 
+  ngOnChanges(changes: SimpleChanges) {
+
+    if (changes['message'] !== undefined)
+      this.message = changes['message'].currentValue;
+
+    if (changes['relation'] !== undefined)
+      this.relation = changes['relation'].currentValue;
+
+    this.Search(this.message, this.relation);
 
   }
 
@@ -101,37 +109,8 @@ export class ResultComponent implements OnInit {
   }
 
   NewSearch(mot: string) {
-
     this.message = mot;
-    this.ngxLoader.startLoader('loader-01');
     this.sendMessage(mot);
-    this.apiWord.getMot(this.message, this.relation).subscribe((res: Mot) => {
-
-      this.res = res;
-      if (this.res.mapEntrantes == null && this.res.mapEntrantes == undefined) {
-        this.EnterEmpty = null;
-      } else {
-        this.res.mapEntrantesNames = Object.keys(this.res.mapEntrantes);
-        for (var e of this.res.mapEntrantesNames) {
-          this.pages[e] = 1;
-        }
-        this.EnterEmpty = Object.keys(this.res.mapEntrantes);
-      }
-
-      if (this.res.mapSortantes == null && this.res.mapSortantes == undefined) {
-        this.SortEmpty = null;
-      } else {
-        this.res.mapSortantesNames = Object.keys(this.res.mapSortantes);
-        for (var a of this.res.mapSortantesNames) {
-          this.pagesSortantes[a] = 1;
-        }
-        this.SortEmpty = Object.keys(this.res.mapSortantes);
-      }
-
-      this.ngxLoader.stopLoader('loader-01');
-
-    })
-
   }
 
 }
